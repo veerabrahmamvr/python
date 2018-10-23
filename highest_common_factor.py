5ec5e658639310c27970204db7eeca8b0e8aa83d
veerabrahmamvr <veerabrahmam_vannemreddi@mentor.com>
2018-10-23 17:55:06 +0530

    modified logic for hcf

--- highest_common_factor.py
+++ highest_common_factor.py
@@ -4,10 +4,10 @@ def hcf (i,j):
     hcf_value = min(i,j)
     if i==j or i%j==0 or j%i==0:
         return hcf_value
-    for k in range (1,hcf_value//2):
+    for k in range (hcf_value//2,0,-1):
         if i%k==0 and j%k==0:
             hcf_value=k
-    return hcf_value
+            break
+    return hcf_value    
 
-    
 print ("{0},{1} highe common factore is {2}".format(sys.argv[1],sys.argv[2],hcf(int(sys.argv[1]),int(sys.argv[2]))))
